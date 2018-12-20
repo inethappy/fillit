4891674e8779bf649ee2464fdf001b2fac704dd1
Maryna KOTYTSKAYA <mkotytsk@e2r3p8.unit.ua>
2018-12-20 16:08:50 +0200

    try2

--- fill/src/tetr_make.c
+++ fill/src/tetr_make.c
@@ -1,10 +1,6 @@
-#include <unistd.h>
 #include <stdio.h>
-#include <fcntl.h>
 #include "libft.h"
 #include "fillit.h"
-
-
 
 t_tetr	**ft_tetr_make(char **tetrs, int count)
 {
@@ -48,7 +44,35 @@ t_tetr	*ft_tetr_new(char *s, int count)
 			new->y[j] = i / 5;
 		}
 	}
+	move_tetr(new);
 	return (new);
+}
+
+void move_tetr(t_tetr *new)
+{
+	int i;
+	int j;
+	int min_x;
+	int min_y;
+
+	i = 0;
+	j = -1;
+	while (++j < 4)
+	{
+		while (++i < 4)
+			{
+				min_x = (new->x[i] <= new->x[i - 1]) ? new->x[i] : new->x[i - 1];
+				min_y = (new->y[i] <= new->y[i - 1]) ? new->y[i] : new->y[i - 1];
+			}
+		min_x = (min_x <= new->x[j]) ? min_x : new->x[j];
+		min_y = (min_y <= new->x[j]) ? min_y : new->x[j];
+	}
+	i = -1;
+	while (++i < 4)
+	{
+		new->x[i] -= min_x;
+		new->y[i] -= min_y;
+	}
 }
 
 void	ft_free_tts(t_tetr **tts)
